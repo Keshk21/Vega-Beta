2b6d1f3f49eef994b01d02956b2b57c0a0b65bdd
Ahmed Nabil <Keshk21@hotmail.com>
2019-03-01 19:16:05 +0200

    seed Feature Table data

---
+++ Migrations/20190301163423_FeatureTableSeeding.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class FeatureTableSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature1-ModelA', 1)");
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature1-ModelB', 1)");
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature1-ModelC', 1)");
+
+             migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature2-ModelA', 2)");
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature2-ModelB', 2)");
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature2-ModelC', 2)");
+
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature3-ModelA', 3)");
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature3-ModelB', 3)");
+            migrationBuilder.Sql("INSERT INTO Feature (FeatureName, ModelId) vALUES('Feature3-ModelC', 3)");
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes");
+
+        }
+    }
+}
---
+++ Models/Feature.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vega.Models
+{
+    [Table("Feature")]
+    public class Feature
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string FeatureName { get; set; }
+        public Model Model { get; set; }
+        public int ModelId { get; set; }
+    }
+}
