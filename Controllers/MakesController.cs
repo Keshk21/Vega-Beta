a1216c10fa096271837b984e1841c7209a78195b
Ahmed Nabil <Keshk21@hotmail.com>
2019-03-01 17:56:11 +0200

    Create the make APT

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Vega.Models;
+using System.Collections.Generic;
+using Vega.Dal;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using AutoMapper;
+using Vega.Controllers.Resources;
+
+namespace Vega.Controllers
+{
+
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext context;
+        private readonly IMapper mapper;
+        public MakesController(VegaDbContext context, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.context = context;
+
+        }
+        [HttpGet("/api/Makes")]
+        public async Task<IEnumerable<MakeResource>> GetMakes()
+        {
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+            return mapper.Map<IList<Make>,List<MakeResource>>(makes);
+
+        }
+    }
+}
