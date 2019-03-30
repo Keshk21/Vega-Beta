19a6202a9f239785a95657042966d90548f9c2fd
Ahmed Nabil <Keshk21@hotmail.com>
2019-03-30 20:22:15 +0200

    map Vehicle to VehicleResource

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -5,29 +5,45 @@ using Microsoft.AspNetCore.Mvc;
 using Vega.Dal;
 using Vega.Models;
 using System.Web.Http;
-
+using Vega.Controllers.Resources;
+using System;
 
 namespace Vega.Controllers
 {
-    [Route("api/[controller]")]
-    public class VehiclesController:Controller
+    [Route("api/Vehicles")]
+    public class VehiclesController : Controller
     {
-        /* private IMapper mapper;
-         private VegaDbContext context;
+         private IMapper mapper;
+        private readonly VegaDbContext context;
 
-          public VehiclesController(VegaDbContext context,IMapper mapper)
-         {
-            this.mapper = mapper;
-             this.context = context;
+        /* private VegaDbContext context;
 
-         }*/
-        [HttpPost("[action]")]
-        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+public VehiclesController(VegaDbContext context,IMapper mapper)
+{
+this.mapper = mapper;
+this.context = context;
+public IMapper Mapper { get;set; }
+
+}*/
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
-            return Ok(vehicle);
+            this.mapper = mapper;
+            this.context= context;
+
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var Vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            Vehicle.LastUpdate= DateTime.Now;
+            this.context.Vehicles.Add(Vehicle);
+             await context.SaveChangesAsync();
+           var Result= mapper.Map<Vehicle,VehcileFeature>(Vehicle);
+
+            return Ok(Result);
         }
 
 
-        
+
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Vega.Controllers.Resources;
 using Vega.Models;
@@ -8,9 +9,28 @@ namespace Vega.Mapping
     {
         public MappingProfile()
         {
+            //Domian to Api resources 
             CreateMap<Make,MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle,VehicleResource>()
+            .ForMember (vr=> vr.Contact,opt=> opt.MapFrom(v=> new ContactResource{Name=v.ContactName, Email=v.ContactEmail,Phone=v.ContactPhone,Adress=v.ContactAdress }))
+            .ForMember(vr=> vr.Features, opt=> opt.MapFrom(v=> v.Features.Select(vf=> vf.FeatureId)));
+                
+        
+
+            // Api Resourced to Domain
+
+            CreateMap<VehicleResource,Vehicle>()
+            .ForMember(v => v.ContactName, opt=> opt.MapFrom(vr=> vr.Contact.Name))
+            .ForMember(v => v.ContactEmail, opt=> opt.MapFrom(vr=> vr.Contact.Email))
+             .ForMember(v => v.ContactPhone, opt=> opt.MapFrom(vr=> vr.Contact.Phone))
+            .ForMember(v => v.ContactAdress, opt=> opt.MapFrom(vr=> vr.Contact.Adress))
+            .ForMember(v => v.Features, opt=> opt.MapFrom(vr=> vr.Features.Select(id=> new VehcileFeature{FeatureId=id})));
+
+
+
+
             
         }
     }
